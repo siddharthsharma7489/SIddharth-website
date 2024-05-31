8cdf4193c4ff9fc57113d3b32369df4f7aac41e6
siddharthsharma7489 <95045002+siddharthsharma7489@users.noreply.github.com>
2024-05-31 23:27:18 +0530

    njnj

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.querySelector(".navbar-toggler");
+    const collapse = document.querySelector(".navbar-collapse");
+  
+    toggler.addEventListener("click", function () {
+      collapse.classList.toggle("show");
+    });
+  });
